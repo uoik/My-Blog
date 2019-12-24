44bff68fa7790d75c7f53e05a3bc5ce27fbd1848
web-小白 <809746386@qq.com>
2019-12-24 17:42:58 +0800

    部署

--- My-Blog/dao/dbutil.js
+++ My-Blog/dao/dbutil.js
@@ -3,7 +3,7 @@ var mysql = require('mysql')
 module.exports.createConnection = () => {
     var connection = mysql.createConnection({
         port: 3306,
-        path: '192.168.56.1',
+        path: '192.172.1.102',
         user: 'root',
         password: 'hui123',
         database: 'My-Blog'
