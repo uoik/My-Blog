0d33af24e6b1b29687d793e1c35097520971d76a
web-小白 <809746386@qq.com>
2020-02-13 15:21:54 +0800

    项目部署

--- My-Blog/dao/dbutil.js
+++ My-Blog/dao/dbutil.js
@@ -2,11 +2,11 @@ var mysql = require('mysql')
 
 module.exports.createConnection = () => {
     var connection = mysql.createConnection({
-        port: 62687,
-        host: 'cd-cdb-b6i1yjbe.sql.tencentcdb.com',
-        user: 'root',
-        password: 'myblog123',
-        database: 'my_blog'
+        port: "3306",
+        host: 'localhost',
+        user: 'my-blog',
+        password: 'EAwrJXWmFmmYbCF5',
+        database: 'my-blog'
     });
     return connection;
 }
